d9b69c71631b0c83c8d2cbbe8101eb71826e6b29
Robert Gutierrez <robgutgom@hotmail.com>
2025-10-22 13:01:41 -0400

    Listo

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
-const isProd = process.env.NODE_ENV === 'production';
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
-  assetPrefix: isProd ? '/reposrossy/' : '',
-  basePath: isProd ? '/reposrossy' : '',
-  output: 'export',
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  allowedDevOrigins: [
+    'http://localhost:3000', // Your primary development origin
+    'http://172.16.0.8:3000', // Any other specific development domains
+    //'*.your-subdomain.dev', // Wildcard for subdomains if needed
+  ],
 };
+
 export default nextConfig;
